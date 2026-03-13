7b784e33a967e87ca09258a4bcd50d04f4fd1dea
waltherlg <rusishss@mail.ru>
2026-03-13 11:34:37 +0600

    fix: UserRegistrationInputDto

--- apps/backend/src/features/auth/api/dto/registration.dto.ts
+++ apps/backend/src/features/auth/api/dto/registration.dto.ts
@@ -1,15 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, Length, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  MinLength,
+} from 'class-validator';
 
 export class UserRegistrationInputDto {
   @ApiProperty({ example: 'email@abc.com' })
+  @IsEmail()
+  @IsNotEmpty()
   @IsString()
   email: string;
   @ApiProperty({ example: 'newUserName' })
+  @IsNotEmpty()
   @IsString()
   @Length(3, 20)
   userName: string;
   @ApiProperty({ example: 'some123PASSWORD' })
+  @IsNotEmpty()
   @IsString()
   @MinLength(6)
   password: string;
