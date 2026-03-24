2003e281b8296147ef1febba0debd0fa3ec46582
Ruslan <rusishss@mail.ru>
2026-03-24 08:50:20 +0600

    feat: add AccountErrorCodeEnum in shared

--- packages/shared-types/src/index.ts
+++ packages/shared-types/src/index.ts
@@ -4,6 +4,7 @@ export type {
   UserNameResponse,
   UserRegistrationInput,
   UserView,
-} from './accounts';
+} from './accounts-api-types';
 export { accountsPaths } from './paths';
 export { SportTypeEnum } from './training-logs';
+export { AccountErrorCodeEnum } from './error-codes';
