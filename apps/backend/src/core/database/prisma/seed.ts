17d3915c14cb7fac19b31a2b43677e31ad6e49a0
waltherlg <rusishss@mail.ru>
2026-04-23 11:09:48 +0600

    wip: prisma seed

---
+++ apps/backend/src/core/database/prisma/seed.ts
@@ -0,0 +1,81 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.event.upsert({
+    where: {
+      sportType_code: {
+        sportType: 'SHOOTING_RIFLE_PISTOL',
+        code: 'AIR_RIFLE_60',
+      },
+    },
+    update: {},
+    create: {
+      sportType: 'SHOOTING_RIFLE_PISTOL',
+      code: 'AIR_RIFLE_60',
+      name: 'Air Rifle 60',
+      resultType: 'SCORE',
+      maxScore: 654,
+      decimals: 1,
+    },
+  });
+
+  await prisma.event.upsert({
+    where: {
+      sportType_code: {
+        sportType: 'SHOOTING_RIFLE_PISTOL',
+        code: 'AIR_RIFLE_40',
+      },
+    },
+    update: {},
+    create: {
+      sportType: 'SHOOTING_RIFLE_PISTOL',
+      code: 'AIR_RIFLE_40',
+      name: 'Air Rifle 40',
+      resultType: 'SCORE',
+      maxScore: 436,
+      decimals: 1,
+    },
+  });
+
+  await prisma.event.upsert({
+    where: {
+      sportType_code: {
+        sportType: 'SHOOTING_RIFLE_PISTOL',
+        code: 'AIR_PISTOL_60',
+      },
+    },
+    update: {},
+    create: {
+      sportType: 'SHOOTING_RIFLE_PISTOL',
+      code: 'AIR_PISTOL_60',
+      name: 'Air Pistol 60',
+      resultType: 'SCORE',
+      maxScore: 600,
+      decimals: 0,
+    },
+  });
+
+  await prisma.event.upsert({
+    where: {
+      sportType_code: {
+        sportType: 'SHOOTING_RIFLE_PISTOL',
+        code: 'AIR_PISTOL_40',
+      },
+    },
+    update: {},
+    create: {
+      sportType: 'SHOOTING_RIFLE_PISTOL',
+      code: 'AIR_PISTOL_40',
+      name: 'Air Pistol 40',
+      resultType: 'SCORE',
+      maxScore: 400,
+      decimals: 0,
+    },
+  });
+}
+
+main()
+  .catch(console.error)
+  .finally(() => prisma.$disconnect());
